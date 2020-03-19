f54d8bd5b72f5699349ca6eca494b512863bab70
Feng Guo <fengsterooni@yahoo.com>
2020-03-19 14:21:42 -0700

    Added Reflection

--- src/main/java/org/example/grpc/calculator/server/CalculatorServer.java
+++ src/main/java/org/example/grpc/calculator/server/CalculatorServer.java
@@ -2,6 +2,7 @@ package org.example.grpc.calculator.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ public class CalculatorServer {
 
         Server server = ServerBuilder.forPort(50051)
                 .addService(new CalculatorServiceImpl())
+                .addService(ProtoReflectionService.newInstance())
                 .build();
 
         server.start();
